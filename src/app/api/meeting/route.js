6f9f6e6ae4325a4f1ba235201a86ec62ed287f78
rana0056 <ranaanees26@gmail.com>
2024-06-29 03:10:00 +0500

    asdad

--- src/app/api/meeting/route.js
+++ src/app/api/meeting/route.js
@@ -4,6 +4,17 @@ import Meeting from '@/app/models/meeting.model';
 import User from '@/app/models/user.model';
 import Therapist from '@/app/models/therapist.model';
 import { NextResponse } from 'next/server';
+import Jwt from 'jsonwebtoken';
+
+const verifyAccessToken = async (accessToken) => {
+    try {
+        const decoded = await Jwt.verify(accessToken, process.env.JWT_SECRET);
+        return decoded;
+
+    } catch (error) {
+        return null
+    }
+}
 
 export async function POST(req) {
     try {
@@ -47,3 +58,30 @@ export async function POST(req) {
         return NextResponse.json({ error: error.message }, { status: 500 });
     }
 }
+
+export async function GET(req) {
+    try {
+        await connectDB();
+
+        const accessToken = await req.nextUrl.searchParams.get("accessToken");
+
+        if (!accessToken) {
+            return NextResponse.json({ error: "Access token not provided" }, { status: 400 });
+        }
+
+        const token = await verifyAccessToken(accessToken);
+
+        if (!token) {
+            return NextResponse.json({ error: "Unauthorized access" }, { status: 401 });
+        }
+
+        const meetings = await Meeting.find({ user: token.id })
+        console.log(meetings);
+        return NextResponse.json({ meetings }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+}
+
+
+
